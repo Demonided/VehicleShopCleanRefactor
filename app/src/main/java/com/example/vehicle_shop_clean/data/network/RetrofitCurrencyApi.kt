a38ee925a7cfda4c2329b441efde64105c889e42
vladimir_matrosov <vladimir.ftsh@mail.ru>
2023-06-04 12:43:43 +0200

    add mapper to ApiResponse

--- app/src/main/java/com/example/vehicle_shop_clean/data/network/RetrofitCurrencyApi.kt
+++ app/src/main/java/com/example/vehicle_shop_clean/data/network/RetrofitCurrencyApi.kt
@@ -11,25 +11,9 @@ class RetrofitCurrencyApi : CurrencyApi {
     override fun getCurrencyRate(
         currency: Currency
     ): ApiResponse<List<Rate>> {
-        val networkResponse = RetrofitClient.api.getLatestRate(currency.abbr).execute()
-        val ratesDto = networkResponse.body()?.rates
-
-        return when {
-            !networkResponse.isSuccessful -> {
-                ApiResponse.Error(networkResponse.errorBody()?.string() ?: DEFAULT_ERROR_MESSAGE)
+        return RetrofitClient.api.getLatestRate(currency.abbr).execute()
+            .mapToApiResponse { getLastRateResponse ->
+                CurrencyRateMapper.map(currency, getLastRateResponse.rates)
             }
-
-            ratesDto != null -> {
-                ApiResponse.Success(CurrencyRateMapper.map(currency, ratesDto))
-            }
-
-            else -> {
-                ApiResponse.Error(DEFAULT_ERROR_MESSAGE)
-            }
-        }
-    }
-
-    companion object {
-        private const val DEFAULT_ERROR_MESSAGE = "Не удалось загрузить курс валют"
     }
 }
